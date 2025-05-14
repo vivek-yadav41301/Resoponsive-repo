4828764bad1c0251f9332e1b158edec2d4e22e79
vivek-yadav41301 <vivekyadav04919@gmail.com>
2025-05-14 15:56:54 +0530

    js change comment

--- port.js
+++ port.js
@@ -40,10 +40,10 @@ ScrollReveal({
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+// ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+// ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
 
 // const typed = new Typed('.multiple-text', {
 //     // strings: ['Frontend Developer'],
